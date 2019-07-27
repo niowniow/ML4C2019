c2e5a52d80c10c4db44e18b97eeaf2c67845de67
matthmey <matthias.meyer@tik.ee.ethz.ch>
2019-07-27 12:00:07 +0200

    added more osc

--- ganductor/user/sketch.js
+++ ganductor/user/sketch.js
@@ -12,9 +12,11 @@ let video;
 const knnClassifier = ml5.KNNClassifier();
 let poseNet;
 let poses = [];
+let canvasX = 640
+let canvasY = 480 
 
 function setup() {
-  const canvas = createCanvas(640, 480);
+  const canvas = createCanvas(canvasX, canvasY);
   canvas.parent('videoContainer');
   video = createCapture(VIDEO);
   video.size(width, height);
@@ -31,6 +33,9 @@ function setup() {
   });
   // Hide the video element, and just show the canvas
   video.hide();
+
+  console.log("Loading the pretrained dataset")
+
 }
 
 function draw() {
@@ -43,6 +48,8 @@ function draw() {
 
 function modelReady(){
   select('#status').html('model Loaded')
+  knnClassifier.load('./myKNN.json', updateCounts);
+
 }
 
 // Add the current frame from the video to the classifier
@@ -73,13 +80,46 @@ function classify() {
 
 let old_command = 'None'
 function sendCommand(command){
-  if (command == old_command){
-    return
+   if (command != old_command){
+      console.log(command);
+      old_command = command
+   }
+
+
+  
+  let num = 0
+  var points = new Array(16);
+  var arrayLength = points.length;
+  for (var i = 0; i < arrayLength; i++) {
+      points[i] = 0.0
   }
-  console.log(command);
-  old_command = command
-  socket.emit('message', '/command/'+command);
+    // Loop through all the poses detected
+    for (let i = 0; i < poses.length; i++) {
+      // For each pose detected, loop through all the keypoints
+      let pose = poses[i].pose;
+      for (let j = 0; j < pose.keypoints.length; j++) {
+        // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+        let keypoint = pose.keypoints[j];
+        // Only draw an ellipse is the pose probability is bigger than 0.2
+        if (keypoint.score > 0.2) {
+          if (num < points.length){
+            points[num] = keypoint.position.x/canvasX 
+            points[num+1] = keypoint.position.y/canvasY
+            num += 2
+
+          }
+        }
+      }
+    }
+
+    message = '/wek/inputs '
+    // message = ''
+    for (var i = 0; i < arrayLength; i++) {
+       message +=  points[i] + ' '
+    }
+  socket.emit('message', message);
   // socket.emit('message', '/1/push1 f, 1.0');
+  // socket.emit('message', '/wek/inputs/ 1.0');
 
 }
 // A util function to create UI buttons
@@ -87,73 +127,6 @@ function createButtons() {
   // When the A button is pressed, add the current frame
   // from the video with a label of "A" to the classifier
   // A util function to create UI buttons
-
-// buttonVUP = select('#addClassVUP');
-//     buttonVUP.mousePressed(function() {
-//       addExample('VUP');
-//     });
-//
-//   buttonVDW = select('#addClassVDW');
-//   buttonVDW.mousePressed(function() {
-//     addExample('VDW');
-//   });
-
-  //   buttonNUP = select('#addClassNUP');
-  // buttonNUP.mousePressed(function() {
-  //   addExample('NUP');
-  // });
-  //
-  //     buttonNDW = select('#addClassNDW');
-  // buttonNDW.mousePressed(function() {
-  //   addExample('NDW');
-  // });
-  //
-  //     buttonGO = select('#addClassGO');
-  // buttonGO.mousePressed(function() {
-  //   addExample('GO');
-  // });
-  //
-  //       buttonSTOP = select('#addClassSTOP');
-  // buttonSTOP.mousePressed(function() {
-  //   addExample('STOP');
-  // });
-
-  // Reset buttons
-  // resetBtnVUP = select('#resetVUP');
-  // resetBtnVUP.mousePressed(function() {
-  //   clearLabel('VUP');
-  // });
-  //
-  // resetBtnVDW = select('#resetVDW');
-  // resetBtnVDW.mousePressed(function() {
-  //   clearLabel('VDW');
-  // });
-
-  // resetBtnNUP = select('#resetNUP');
-  // resetBtnNUP.mousePressed(function() {
-  //   clearLabel('NUP');
-  // });
-  //
-  //   resetBtnNDW = select('#resetNDW');
-  // resetBtnNDW.mousePressed(function() {
-  //   clearLabel('NDW');
-  // });
-  //
-  //     resetBtnGO = select('#resetGO');
-  // resetBtnGO.mousePressed(function() {
-  //   clearLabel('GO');
-  // });
-  //       resetBtnSTOP = select('#resetSTOP');
-  // resetBtnSTOP.mousePressed(function() {
-  //   clearLabel('STOP');
-  // });
-  // Predict button
-  // buttonPredict = select('#buttonPredict');
-  // buttonPredict.mousePressed(classify);
-  //
-  // // Clear all classes button
-  // buttonClearAll = select('#clearAll');
-  // buttonClearAll.mousePressed(clearAllLabels);
 
   // Load saved classifier dataset
 buttonSetData = select('#load');
@@ -181,12 +154,6 @@ function gotResults(err, result) {
       select('#confidence').html(`${confidences[result.label] * 100} %`);
     }
 
-    // select('#confidenceVUP').html(`${confidences['VUP'] ? confidences['VUP'] * 100 : 0} %`);
-    // select('#confidenceVDW').html(`${confidences['VDW'] ? confidences['VDW'] * 100 : 0} %`);
-    // select('#confidenceNUP').html(`${confidences['NUP'] ? confidences['NUP'] * 100 : 0} %`);
-    // select('#confidenceNDW').html(`${confidences['NDW'] ? confidences['NDW'] * 100 : 0} %`);
-    // select('#confidenceGO').html(`${confidences['GO'] ? confidences['GO'] * 100 : 0} %`);
-    // select('#confidenceSTOP').html(`${confidences['STOP'] ? confidences['STOP'] * 100 : 0} %`);
   }
 
   try {
@@ -200,12 +167,6 @@ function gotResults(err, result) {
 function updateCounts() {
   const counts = knnClassifier.getCountByLabel();
 
-  // select('#exampleVUP').html(counts['VUP'] || 0);
-  // select('#exampleVDW').html(counts['VDW'] || 0);
-  // select('#exampleNUP').html(counts['NUP'] || 0);
-  // select('#exampleNDW').html(counts['NDW'] || 0);
-  // select('#exampleGO').html(counts['GO'] || 0);
-  // select('#exampleSTOP').html(counts['STOP'] || 0);
   classify()
 }
 
@@ -280,8 +241,8 @@ socket.on('connect', function() {
                  host: '127.0.0.1'
              },
              client: {
-                 port: 5005,
-                 host: '127.0.0.1'
+                 port: 6448,
+                 host: '192.168.10.144'
              }
          }
      );
